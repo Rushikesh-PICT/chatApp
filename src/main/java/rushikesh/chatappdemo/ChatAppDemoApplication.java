f46bd6a10715136b9e2ffa243176a19047f88951
Rushikesh Sarode <rushikeshsarode@rhythmflows.com>
2025-12-14 16:59:39 +0530

    initial setup

---
+++ src/main/java/rushikesh/chatappdemo/ChatAppDemoApplication.java
@@ -0,0 +1,13 @@
+package rushikesh.chatappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rushikesh/chatappdemo/ChatAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rushikesh.chatappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
